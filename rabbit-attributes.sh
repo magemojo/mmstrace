254519aa06b54cd0f6cdc2522625d43ebd06e7a9
geekgirlmm <40037425+geekgirlmm@users.noreply.github.com>
2021-07-22 16:39:01 -0400

    Update rabbit-attributes.sh

--- rabbit-attributes.sh
+++ rabbit-attributes.sh
@@ -3,3 +3,6 @@ sed -i 's/connection="db"/connection="amqp"/g' ./vendor/magento/module-catalog/e
 sed -i 's/name="db"/name="amqp"/g' ./vendor/magento/module-catalog/etc/queue_publisher.xml
 sed -i 's/connection="db"/connection="amqp"/g' ./vendor/magento/module-catalog/etc/queue_topology.xml
 php bin/magento cache:flush;redis-cli -h redis flushall;/usr/share/stratus/cli cache.varnish.clear;/usr/share/stratus/cli cache.cloudfront.invalidate
+rm ./rabbit-attributes.sh
+echo "Update to product_action_attribute.update & product_action_attribute.website.update for RabbitMQ broker complete, if no errors above"
+echo "Please verify that env.php contains correct rabbit creds & product_action_attribute.update & product_action_attribute.website.update consumers, otherwise they will not run"
