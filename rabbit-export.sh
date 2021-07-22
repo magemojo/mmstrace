797e1decffd154fcf26fce2a23b4b8ebc23d991a
geekgirlmm <40037425+geekgirlmm@users.noreply.github.com>
2021-07-22 16:36:14 -0400

    Update rabbit-export.sh

--- rabbit-export.sh
+++ rabbit-export.sh
@@ -3,3 +3,6 @@ sed -i 's/connection="db"/connection="amqp"/g' ./vendor/magento/module-import-ex
 sed -i 's/name="db"/name="amqp"/g' ./vendor/magento/module-import-export/etc/queue_publisher.xml
 sed -i 's/connection="db"/connection="amqp"/g' ./vendor/magento/module-import-export/etc/queue_topology.xml
 php bin/magento cache:flush;redis-cli -h redis flushall;/usr/share/stratus/cli cache.varnish.clear;/usr/share/stratus/cli cache.cloudfront.invalidate
+rm ./rabbit-export.sh
+echo "Update to exportProcessor for RabbitMQ broker complete, if no errors above"
+echo "Please verify that env.php contains correct rabbit creds & exportProcessor consumer, otherwise it will not run"
