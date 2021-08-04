e745e95348c23db64e94f917100a48d1a559b361
jackie <jackie@magemojo.com>
2021-08-03 23:27:39 -0400

    hop updater

--- hopupdate.py
+++ hopupdate.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
-# Abusive IP Grabber from MageMojo HOP Udater 1.0
+# Abusive IP Grabber from MageMojo HOP Updater 1.0
 
+import os
+import time
+
+# Wait 2 seconds to be sure hop.py has closed
+time.sleep(2)
+
+# copy latest file to hop.py
+moveit = "mv ./hop.check /srv/.nginx/hop.py"
+os.system(moveit)
+
+remove = "rm ./hopupdate.py"
+os.system(remove)
